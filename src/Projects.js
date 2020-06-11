70538edab0bdec882c2bca580f1ad23a10134a79
tommyrharper <tommyrharper@gmail.com>
2020-06-11 19:28:39 +0100

    Create basic grid

---
+++ src/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Projects extends React.Component {
+  render() {
+    return <div class="grid-container">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+    </div>
+  }
+}
+
+export default Projects;
