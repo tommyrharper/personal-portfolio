0412e5c585b69721218e822ed327ab55df43bb1f
tommyrharper <tommyrharper@gmail.com>
2020-06-12 16:18:43 +0100

    Add BrowserRouter to App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import Projects from './Projects';
-import './App.css'
+import Projects from './components/Projects';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/Home'
 
 class App extends React.Component {
   render() {
-    return <div>
-      <h1>Hey, I'm Tom.</h1>
-      <h2>I'm a software developer who loves making things.</h2>
-      <Projects/>
+    return(
+      <div>
+        <BrowserRouter>
+          <div>
+            <Navigation/>
+              <Switch> 
+                <Route exact path="/" component={Home}/>
+                <Route path="/Projects" component={Projects}/>
+              </Switch>
+          </div> 
+        </BrowserRouter>
       </div>
+    ) 
   }
 }
 
