8188777c750b56eed62ab48948f8bb6189789244
tommyrharper <tommyrharper@gmail.com>
2020-06-12 15:36:18 +0100

    Expand projects divisions

--- src/Projects.js
+++ src/Projects.js
@@ -6,11 +6,32 @@ class Projects extends React.Component {
       <div class="projects-container">
         <div class="paddingBlock">
           <div class="projects">
-            <div class="project">Mission-Ctrl</div>
-            <div class="project">Bank Tech Test</div>
-            <div class="project">Thermostat</div>
-            <div class="project">Bowling-Scorecard</div>
-            <div class="project">Rock Paper Scissors</div>
+            <div class="project" onClick="www.google.com">Mission-Ctrl
+            </div>
+            <div class="project">
+              Bank Tech Test
+            </div>
+            <div class="project">
+              Thermostat
+            </div>
+            <div class="project">
+              Bowling-Scorecard
+            </div>
+            <div class="project">
+              Acebook-myspace
+            </div>
+            <div class="project">
+              Twitter
+            </div>
+            <div class="project">
+              Rock Paper Scissors
+            </div>
+            <div class="project">
+              Takeaway-Service
+            </div>
+            <div class="project">
+              Airport
+            </div>
           </div>
         </div>
       </div>
