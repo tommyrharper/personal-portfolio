67de00389d2ff0d15e9419a4c9257374c46024d3
tommyrharper <tommyrharper@gmail.com>
2020-06-12 17:13:24 +0100

    Add history to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import Projects from './components/Projects';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './components/Home'
+import Bank from './components/projects/Bank'
 
 class App extends React.Component {
   render() {
     return(
       <div>
-        <BrowserRouter>
           <div>
             <Navigation/>
               <Switch> 
                 <Route exact path="/" component={Home}/>
                 <Route path="/Projects" component={Projects}/>
+                <Route exact path="/bank" component={Bank}/>
               </Switch>
           </div> 
-        </BrowserRouter>
       </div>
     ) 
   }
 }
 
-export default App;
+export default withRouter(App);
