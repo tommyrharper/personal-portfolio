fb615568f1afc7e15b395cb2578b9de44d22a187
tommyrharper <tommyrharper@gmail.com>
2020-06-23 15:46:43 +0100

    Add Airport.js text

--- src/components/projects/Airport.js
+++ src/components/projects/Airport.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Airport extends React.Component {
   render() {
     return(
-      <div>Airport</div>
+      <div>Airport
+        <p>In this project I built the business logic for an airport control command line application.</p>
+      </div>
     ) 
   }
 }
